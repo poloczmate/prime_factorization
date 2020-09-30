a1cb7401d6dfab89607ef6058b7ab13fe4b57f63
poloczmate <48856366+poloczmate@users.noreply.github.com>
2020-09-30 21:05:27 +0200

    added function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 
 bool isPrime(int n){
     int counter = 0;
     for(int i = 1; i <= n; i++){
-        if(n % 1 == 0){
+        if(n % i == 0){
             counter++;
         }
     }
@@ -18,5 +19,19 @@ int main(){
     int number;
     std::cout << "Number: ";
     std::cin >> number;
-
+    int n = number;
+    std::vector<int> v;
+    while(n != 1){
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i) && n % i == 0){
+                v.push_back(i);
+                n = n/i;
+                i = n;
+            }
+        }
+    }
+    for(auto i = v.begin(); i != v.end(); ++i){
+        std::cout << *i << std::endl;
+    }
+    return 0;
 }
